eb1b3397fabbf529faff1e8406bb3b80d497faaf
ilBronza <davide_bronzini@hotmail.com>
2025-10-12 19:24:30 +0200

    all

--- src/MeasurementUnits.php
+++ src/MeasurementUnits.php
@@ -14,7 +14,7 @@ class MeasurementUnits implements RoutedObjectInterface
             return;
 
         $settingsButton = $menu->provideButton([
-                'text' => 'generals.settings',
+                'text' => 'menu::menu.settings',
                 'name' => 'settings',
                 'icon' => 'gear',
                 'roles' => ['administrator']
