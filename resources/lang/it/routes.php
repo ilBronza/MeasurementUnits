ac5480831d3fd452f18cc77e9baf84d7c0d19df0
ilBronza <davide_bronzini@hotmail.com>
2026-08-07 22:05:33 +0200

    Add Italian translations for missing package route page titles.
    Fill routes.php from the design-time audit so named package routes resolve titles without missingtranslations noise.

---
+++ resources/lang/it/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'measurementUnitsmeasurementUnits' => [
+		'index' => 'Lista elemento',
+		'create' => 'Crea elemento',
+		'store' => 'Salva elemento',
+		'show' => 'Mostra elemento :model',
+		'edit' => 'Modifica elemento :model',
+		'update' => 'Aggiorna elemento',
+		'destroy' => 'Elimina elemento',
+	],
+];
